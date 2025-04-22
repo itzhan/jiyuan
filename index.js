e7cca1979cde1831c38ced7bcb62c75624bc823d
itzhan <2802965114@qq.com>
2025-04-22 13:58:56 +0800

    测试:公众号监听功能

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express')
+const cloud = require("wx-server-sdk");
+
+cloud.init({
+  env: 'cloud1-9g0ddevqa589d711'
+})
 
 const app = express()
-
 app.use(express.json())
 
 app.post('/', async (req, res) => {
-  console.log('消息推送', req.body)
+  if (req.body.type === 'unsubscribe' || req.body.type === 'subscribe') {
+    const wxContext = cloud.getWXContext()
+    console.log('wxContext', wxContext)
+
+    const db = cloud.database()
+    const user = await db.collection('users').where({
+      openid: wxContext.OPENID
+    }).get()
+    console.log('user', user)
+
+    if (user.data.length > 0) {
+      await db
+      .collection("users")
+      .doc(user.data[0]._id)
+      .update({
+        data: {
+          gzhOpenId:
+            req.body.type === "subscribe" ? req.body.FromUserName : null,
+        },
+      });
+    }
+  }
+
   res.send('success') // 不进行任何回复，直接返回success，告知微信服务器已经正常收到。
 });
 
